05131c3a3d228eee96808bac6898da11e89ff249
x <>
2024-03-09 19:28:43 +0000

    rename message param to initial_message

--- assistant_frontend.py
+++ assistant_frontend.py
@@ -2,10 +2,10 @@ import requests
 import gradio as gr
 import os
 
-def process_text(recipient_number, message, goal):
+def process_text(recipient_number, initial_message, goal):
   # Send data to Node.js backend
   url = "http://localhost:3000/sends-message"  # Adjust URL based on your setup
-  response = requests.post(url, json={"number": recipient_number, "message": message, "additional": goal})
+  response = requests.post(url, json={"number": recipient_number, "message": initial_message, "additional": goal})
   response.raise_for_status()  # Raise an error for non-200 status codes
   data = response.json()
   return data["msg"]
